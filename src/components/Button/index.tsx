68c481002721ccb19ee95cb3f091fd2e2fcf4da4
tboirin <tboirin@partenaire-intersport.fr>
2023-12-08 11:25:09 +0100

    fix button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -10,7 +10,7 @@ export default function Button({ children, red, blue, white, onClick }: Props) {
   return (
     <button
       type="button"
-      className={`py-3 px-4 text-white rounded-basic duration-300 ${
+      className={`py-3 px-4 rounded-basic duration-300 ${
         red ? 'bg-red hover:bg-blue ' : ''
       } ${blue ? 'bg-blue hover:bg-red ' : ''} ${
         white ? 'bg-white text-black hover:bg-transparent border border-white hover:text-white' : ''
