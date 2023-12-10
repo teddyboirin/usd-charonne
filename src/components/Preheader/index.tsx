b0b554ccce07f77ec1ceff6cd3b3c87e1e24d557
tboirin <tboirin@partenaire-intersport.fr>
2023-12-10 23:03:24 +0100

    add swiper on preheader

--- src/components/Preheader/index.tsx
+++ src/components/Preheader/index.tsx
@@ -1,9 +1,36 @@
+'use client';
+import { Autoplay, Navigation } from 'swiper/modules';
 import { memo } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/effect-fade';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 function Preheader() {
   return (
-    <div className="absolute top-0 w-full p-0.5 md:p-1.5 bg-dark-1 text-white z-50 text-11 md:text-14 flex items-center justify-center">
-      <p>Recherchons joueurs, joueuses et entraineurs</p>
+    <div className="absolute top-0 flex items-center justify-center w-full p-1.5 bg-dark-1 text-white z-50 text-11 md:text-14 max-h-[40px]">
+      <Swiper
+        slidesPerView={1}
+        direction="vertical"
+        autoplay={{
+          delay: 4500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, Navigation]}
+        className="preheader-swiper w-full md:w-[45%] max-h-[inherit]"
+      >
+        <SwiperSlide className="text-center flex items-center justify-center">
+          <p className="h-full text-center flex items-center justify-center">
+            Recherchons joueurs, joueuses et entraineurs
+          </p>
+        </SwiperSlide>
+        <SwiperSlide className="text-center flex items-center justify-center">
+          <p className="h-full text-center flex items-center justify-center">
+            Lorem, ipsum dolor sit amet consectetur adipisicing.
+          </p>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
