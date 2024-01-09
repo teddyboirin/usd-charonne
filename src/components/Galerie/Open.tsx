d747e82a61a5a3f485778ff0b533dfddf8560f42
tboirin <tboirin@partenaire-intersport.fr>
2024-01-09 18:57:06 +0100

    update

--- src/components/Galerie/Open.tsx
+++ src/components/Galerie/Open.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function Open({ imageLink, title, onClick }) {
   return (
-    <div className="h-screen w-screen bg-filter z-[100] fixed !top-0 !left-0 flex items-center justify-center m-0">
+    <div className="h-screen !w-screen bg-filter z-[100] fixed !top-0 !left-0 flex items-center justify-center m-0">
       <div className="right-4 top-4 absolute text-white cursor-pointer h-[20px] w-[20px] flex items-center justify-center">
         <button type="button" onClick={onClick}>
           <Arrow />
