39df77802ad962dd4eb227924ddd81174b13d1b8
eden bergel <edenbergel@edens-mbp.home>
2024-01-10 22:49:47 +0100

    filter galerie on click event & style buttons

--- src/components/Galerie/ButtonFilter.tsx
+++ src/components/Galerie/ButtonFilter.tsx
@@ -1,15 +1,16 @@
-import { memo } from 'react';
+import { memo } from "react";
 
 interface Props {
   value: string;
   onClick?: () => void;
+  isActive: boolean;
 }
 
-function ButtonFilter({ value, onClick }: Props) {
+function ButtonFilter({ value, onClick, isActive }: Props) {
   return (
     <button
       type="button"
-      className="bg-dark-1 text-white rounded-basic py-2 px-3 text-12 hover:bg-white hover:text-dark-1 duration-300"
+      className={`border border-black rounded-basic py-2 px-3 text-12 hover:bg-white hover:text-dark-1 duration-300 ${isActive ? 'bg-white text-dark-1' : 'bg-dark-1 text-white'}`}
       onClick={onClick}
     >
       {value}
--- src/components/Galerie/index.tsx
+++ src/components/Galerie/index.tsx
@@ -5,23 +5,31 @@ import Item from './Item';
 import ButtonFilter from './ButtonFilter';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import Masonry from '@mui/lab/Masonry';
+import { useState } from 'react';
 
 export default function GlobalContainer({ data }) {
   const { width } = useWindowSize();
+  const [teamType, setTeamType] = useState('');
+
+  const filteredTeams = data?.data.filter(teamTypes => {
+    const { team } = teamTypes.attributes;
+    const teamTypesFiltered = team.toLowerCase().includes(teamType.toLowerCase());
+
+    return teamTypesFiltered;
+  })
 
   return (
     <>
       <section className="w-full bg-gray-0">
         <div className="w-full flex items-center justify-center">
           <div className="flex gap-3 items-center justify-center max-w-[80%] md:max-w-[600px]">
-            <ButtonFilter value="SM1" />
-            <ButtonFilter value="SF1" />
-            <ButtonFilter value="S1" />
+            <ButtonFilter value="SM1" onClick={() => setTeamType("SM1")} isActive={teamType === "SM1"} />
+            <ButtonFilter value="SF1" onClick={() => setTeamType("SF1")} isActive={teamType === "SF1"} />
           </div>
         </div>
         <div className="mt-6 w-full">
           <Masonry columns={isDesktop(width) ? 4 : 2} spacing={2}>
-            {data?.data?.map((item, index) => (
+            {filteredTeams?.map((item, index) => (
               <Item
                 key={index}
                 imageLink={`${
