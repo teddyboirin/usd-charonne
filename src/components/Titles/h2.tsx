34018cc622efb254422840903f33b4bed5ee000f
tboirin <tboirin@partenaire-intersport.fr>
2024-01-12 17:36:46 +0100

    add uppercase

--- src/components/Titles/h2.tsx
+++ src/components/Titles/h2.tsx
@@ -3,18 +3,19 @@ import { memo } from 'react';
 interface Props {
   title: string;
   className?: string;
+  uppercase?: string;
   center?: boolean;
   color?: 'black' | 'white';
 }
 
-function H2({ title, center, color = 'white', className }: Props) {
+function H2({ title, center, color = 'white', className, uppercase }: Props) {
   return (
     <h3
       className={`${className || ''} text-3xl md:text-5xl ${
         color === 'black' ? 'text-dark-1' : ''
       } ${color === 'white' ? 'text-white' : ''} ${
         center ? 'text-center' : ''
-      }`}
+      } ${uppercase ? 'uppercase' : ''}`}
     >
       {title}
     </h3>
